dbd78638cda30ca8562a22fefdbf2243cf13cf85
JenilVirani <82446102+JenilVirani@users.noreply.github.com>
2021-04-13 19:45:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def demo():
-    return "HELLO"
+    return "<html><head><body>HELLO</body></head></html>"
 
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=8080)
